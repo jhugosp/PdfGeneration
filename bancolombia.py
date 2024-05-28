21ae5c9343387f2f34ff9f737fc573a136c88ba2
jhugosp <juan.huguet@wizeline.com>
2024-05-28 14:50:12 -0500

    ADD: randomized values generation funcs

--- bancolombia.py
+++ bancolombia.py
@@ -1,7 +1,9 @@
 from jinja2 import Environment, FileSystemLoader
-from templates.bancolombia_model import account_state_template, summary_template, row_template
+from templates.bancolombia_model import Bancolombia
 import random
 import datetime
+
+model_templates = Bancolombia()
 
 
 def give_date(transactions_amount):
@@ -26,9 +28,31 @@ def give_date(transactions_amount):
 def create_descriptions(transactions_amount):
     descriptions = []
     for _ in range(transactions_amount):
-        descriptions.append(f"Description_{_}")
+        descriptions.append(f"Description_{_ + 1}")
 
     return descriptions
+
+
+def create_branches(transaction_amount):
+    branches = []
+    branch_template: list = model_templates.branches_templates()
+    for _ in range(transaction_amount):
+        branches.append(random.choice(branch_template))
+
+    return branches
+
+
+def generate_formatted_number(transactions_amount):
+
+    values_transactions = []
+    min_value = 0.01
+    max_value = 9_999_999.99
+
+    for _ in range(transactions_amount):
+        random_number = random.uniform(min_value, max_value)
+        values_transactions.append(f"{random_number:,.2f}")
+
+    return values_transactions
 
 
 def write_results_to_file(template_out):
@@ -39,20 +63,17 @@ def write_results_to_file(template_out):
     print("HTML file generated successfully!")
 
 
-account_state = account_state_template()
-
-summary = summary_template()
-
+account_state = model_templates.account_state_template()
+summary = model_templates.summary_template()
 table_rows = [
-    row_template()
+    model_templates.row_template()
 ]
-
 
 if __name__ == "__main__":
     file_loader = FileSystemLoader('static')
     env = Environment(loader=file_loader)
     template = env.get_template('sample_banco_bogota.html')
-
-    print(f"dates is: {give_date(10)}")
+    transactions = 10
+    print(f"{give_date(transactions)} \n{create_descriptions(transactions)} \n{create_branches(transactions)} \n")
 
     # write_results_to_file(template_out=template)
