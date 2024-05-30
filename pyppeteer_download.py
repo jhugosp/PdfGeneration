e1f9ed4e4f131244ce125a0dad1ae306a906af90
jhugosp <juan.huguet@wizeline.com>
2024-05-30 09:19:22 -0500

    ADD: printing on console

--- pyppeteer_download.py
+++ pyppeteer_download.py
@@ -26,7 +26,7 @@ async def save_page_as_pdf(url, n, output_dir):
         }''')
         pdf_path = os.path.join(output_dir, f"extracts_{i+1}.pdf")
         await page.pdf({'path': pdf_path, 'format': 'A4', 'printBackground': True})
-        print("Saved PDF {}".format(i))
+        print("Saved PDF {}".format(i + 1))
         await page.reload()
 
     await browser.close()
