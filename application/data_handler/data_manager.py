0c615711dd4dba28f8f9e28e641253a0f035f63d
jhugosp <juan.huguet@wizeline.com>
2024-06-11 15:16:37 -0500

    REMOVE: unnecessary method

--- application/data_handler/data_manager.py
+++ application/data_handler/data_manager.py
@@ -1,6 +1,5 @@
 from domain.models.bancolombia_model import Bancolombia
 import uuid
-import random
 import json
 
 model_templates = Bancolombia()
@@ -28,46 +27,3 @@ class DataManager:
         except IOError as e:
             print("Something went wrong: {}".format(str(e)))
 
-    @staticmethod
-    def prepare_pdf_information() -> tuple:
-        """ PDF information preparation function, in charge of generating:
-
-            - Dates
-            - Branches
-            - Balances and transactions operations
-            - Interest calculation
-            - Total transactions
-            - Operations summary
-            - Account state formation
-
-            :return: A tuple containing the final rows, summary and account state to present to client.
-        """
-        initial_balance = model_templates.summary_template().get("previous_balance")
-        transactions = random.randint(5, 10)
-
-        dates = model_templates.create_dates(transactions)
-
-        branches = model_templates.create_branches(transactions)
-
-        balances, transactions_values, descriptions = (
-            model_templates.calculate_balance(transactions, initial_balance))
-
-        final_template_rows, total_interest = model_templates.aggregate_rows_information(
-            dates=dates,
-            descriptions=descriptions,
-            branches=branches,
-            balances=balances,
-            transactions_values=transactions_values,
-            transactions_amount=transactions,
-        )
-
-        final_summary = model_templates.create_summary(
-            balances=balances,
-            transactions_values=transactions_values,
-            transactions_amount=transactions,
-            first_balance=initial_balance,
-            interests=total_interest)
-
-        final_account_state = model_templates.create_account_state()
-
-        return final_template_rows, final_summary, final_account_state
