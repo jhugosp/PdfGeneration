11ed61738340102b5ebfe9d2d5bd97b9752fc11e
jhugosp <juan.huguet@wizeline.com>
2024-06-17 14:49:02 -0500

    ADD: code property

--- domain/models/documents/base_document.py
+++ domain/models/documents/base_document.py
@@ -1,5 +1,6 @@
 class BaseDocument:
-    def __init__(self, structured_meta, raw_meta):
+    def __init__(self, structured_meta, raw_meta, code):
+        self._code = code
         self._structured_meta = structured_meta
         self._raw_meta = raw_meta
 
