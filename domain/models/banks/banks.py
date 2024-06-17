8d8009f4aa6686f4e9395a2682fd0169709738f5
jhugosp <juan.huguet@wizeline.com>
2024-06-17 17:44:35 -0500

    REFACTOR: file name

--- domain/models/banks/banks.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-
-class Banks(Enum):
-    BANCOLOMBIA = "BANCOLOMBIA"
-    BANCO_BOGOTA = "BANCO_BOGOTA"
-    BBVA = "BBVA"
-    COLPATRIA = "COLPATRIA"
-    CAJA_SOCIAL = "CAJA_SOCIAL"
---
+++ domain/models/banks/banks_types.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+
+class Banks(Enum):
+    BANCOLOMBIA = "bancolombia"
+    BANCO_BOGOTA = "banco-bogota"
+    BBVA = "bbva"
+    COLPATRIA = "colpatria"
+    CAJA_SOCIAL = "caja-social"
