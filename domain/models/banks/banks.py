e87b7b62db5871ad8c800bc05e3153e9e06ffd8b
jhugosp <juan.huguet@wizeline.com>
2024-06-17 14:48:23 -0500

    ADD: new enum

---
+++ domain/models/banks/banks.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+
+class Banks(Enum):
+    BANCOLOMBIA = "BANCOLOMBIA"
+    BANCO_BOGOTA = "BANCO_BOGOTA"
+    BBVA = "BBVA"
+    COLPATRIA = "COLPATRIA"
+    CAJA_SOCIAL = "CAJA_SOCIAL"
