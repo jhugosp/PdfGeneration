4260029c7ce3bbc13b5ed3b8341918af7e39357c
jhugosp <juan.huguet@wizeline.com>
2024-06-17 16:03:55 -0500

    ADD: dtos

---
+++ application/dto/caja_social_dto.py
@@ -0,0 +1,17 @@
+class CajaSocialDto:
+    def __init__(self, metadata, code, rules):
+        self._metadata = metadata
+        self._code = code
+        self._rules = rules
+
+    @property
+    def code(self):
+        return self._code
+
+    @property
+    def metadata(self):
+        return self._metadata
+
+    @property
+    def rules(self):
+        return self._rules
