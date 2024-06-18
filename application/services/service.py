a0c6f172e1d8f4189b0356534abec9ebe765e28a
jhugosp <juan.huguet@wizeline.com>
2024-06-18 17:17:16 -0500

    ADD: services classes

---
+++ application/services/service.py
@@ -0,0 +1,14 @@
+from domain.models.repository import Repository
+from application.data_handler.dto_generator import DtoGenerator
+
+
+class Service:
+    def __init__(self, dto_generator: DtoGenerator, repository: Repository):
+        self._dto_generator = dto_generator
+        self._repository = repository
+
+    def get_one(self, doc_code, bank_type):
+        pass
+
+    def get_all(self, bank_type):
+        pass
