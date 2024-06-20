61650675e97c998a91349b8f1e80990e1949e3f6
jhugosp <juan.huguet@wizeline.com>
2024-06-20 11:20:24 -0500

    REFACTOR: pass down data to service poarent

--- application/services/service.py
+++ application/services/service.py
@@ -7,11 +7,8 @@ class Service:
         self._dto_generator = dto_generator
         self._repository = repository
 
-    def get_one(self, doc_code, bank_type):
+    def get_one(self, structured, raw, doc_code, rules, bank_type):
         pass
 
-    def get_all(self, bank_type):
-        pass
-
-    def get_multiple(self, doc_codes, bank_type):
+    def get_multiple(self, structured, raw, doc_codes, rules, bank_type):
         pass
