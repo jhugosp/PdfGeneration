daaecd4aae6a53de2b0202a480ada87790581a7e
jhugosp <juan.huguet@wizeline.com>
2024-06-20 11:22:22 -0500

    REFACTOR: pass down data to service poarent

--- application/services/bancolombia_service.py
+++ application/services/bancolombia_service.py
@@ -7,19 +7,13 @@ class BancolombiaService(Service):
     def __init__(self, dto_generator: DtoGenerator, repository: Repository):
         super().__init__(dto_generator, repository)
 
-    def get_one(self, doc_code, bank_type):
-        return self._dto_generator.generate_dto(self._repository.get_one(doc_code), bank_type)
+    def get_one(self, structured, raw, doc_code, rules, bank_type):
+        return self._dto_generator.generate_dto(self._repository.get_one(structured, raw, doc_code, rules, bank_type))
 
-    def get_multiple(self, doc_codes, bank_type):
-        entities = self._repository.get_multiple(doc_codes)
+    def get_multiple(self, structured, raw, doc_codes, rules, bank_type):
+        entities = self._repository.get_multiple(structured, raw, doc_codes, rules, bank_type)
         result = []
         for entity in entities:
-            result.append(self._dto_generator.generate_dto(entity, bank_type))
+            result.append(self._dto_generator.generate_dto(entity))
         return result
 
-    def get_all(self, bank_type):
-        entities = self._repository.get_all()
-        result = []
-        for entity in entities:
-            result.append(self._dto_generator.generate_dto(entity, bank_type))
-        return result
